988d890595dab3ee0d749b324aa5cb91e79b7c91
brandon.authenreith <brandonauth@gmail.com>
2020-05-04 18:30:36 -0400

    final Product

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,24 +4,37 @@ const teamoneshootbutton = document.querySelector("#teamone-shoot-button")
 
 
 teamoneshootbutton.addEventListener ("click", function(){
-    let NewOneShot = Number(teamonenumshots.innerHTML) + 1;
-    teamonenumshots.innerHTML = NewOneShot;
-    let ShotChance = Math.random()
-    if (Math.random() < .5) {
+    let newOneShot = Number(teamonenumshots.innerHTML) + 1;
+    teamonenumshots.innerHTML = newOneShot;
+    let shotChance = Math.random()
+    if (shotChance < .5) {
         teamonenumgoals.innerText = Number(teamonenumgoals.innerHTML) + 1 
     }
 })
 
-const teamonenumshots = document.querySelector("#teamone-numshots")
-const teamonenumgoals = document.querySelector("#teamone-numgoals")
-const teamoneshootbutton = document.querySelector("#teamone-shoot-button")
+const teamTwoNumShots = document.querySelector("#teamtwo-numshots")
+const teamTwoNumGoals = document.querySelector("#teamtwo-numgoals")
+const teamTwoShootButton = document.querySelector("#teamtwo-shoot-button")
 
 
-teamoneshootbutton.addEventListener ("click", function(){
-    let NewOneShot = Number(teamonenumshots.innerHTML) + 1;
-    teamonenumshots.innerHTML = NewOneShot;
-    let ShotChance = Math.random()
-    if (Math.random() < .5) {
-        teamonenumgoals.innerText = Number(teamonenumgoals.innerHTML) + 1 
+teamTwoShootButton.addEventListener ("click", function(){
+    let newTwoShot = Number(teamTwoNumShots.innerHTML) + 1;
+    teamTwoNumShots.innerHTML = newTwoShot;
+    let shotTwoChance = Math.random()
+    if (shotTwoChance < .5) {
+        teamTwoNumGoals.innerText = Number(teamTwoNumGoals.innerHTML) + 1 
     }
+})
+
+const resetButton = document.querySelector("#reset-button")
+const numResets = document.querySelector("#num-resets")
+
+resetButton.addEventListener ("click", function() {
+   numResets.innerHTML = Number(numResets.innerHTML) +1;
+   console.log (teamTwoNumGoals.innerHTML , "teamTwoNumGoals")
+   teamTwoNumGoals.innerHTML = "0" ;
+   teamTwoNumShots.innerHTML= "0" ;
+   teamonenumshots.innerHTML = "0" ;
+   teamonenumgoals.innerHTML = "0" ;
+
 })
